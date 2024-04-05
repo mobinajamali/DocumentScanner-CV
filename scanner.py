8c1f1bdfd9b327cac12890ef032f90461d075dd4
mobinajamali <mobina.jamali.mj@gmail.com>
2024-04-05 17:44:11 -0400

    add stackimages utils

--- scanner.py
+++ scanner.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2 as cv
+from utils import stackImages
 
 # 1. CAPTURE WEBCAM
 cap = cv.VideoCapture(0)
@@ -101,47 +102,6 @@ def getWarp(img, biggest):
     imgCropped = cv.resize(imgCropped, (FRAME_WIDTH, FRAME_HEIGHT))
 
     return imgCropped
-
-
-# 7. STACKING IMAGE RESULTS
-def stackImages(scale, imgArray, names):
-    rows = len(imgArray)
-    cols = len(imgArray[0])
-    rowsAvailable = isinstance(imgArray[0], list)
-    width = imgArray[0][0].shape[1]
-    height = imgArray[0][0].shape[0]
-
-    if rowsAvailable:
-        for x in range(0, rows):
-            for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
-                    imgArray[x][y] = cv.resize(imgArray[x][y], (0, 0), None, scale, scale)
-                else:
-                    imgArray[x][y] = cv.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]),
-                                               None, scale, scale)
-                if len(imgArray[x][y].shape) == 2:
-                    imgArray[x][y] = cv.cvtColor(imgArray[x][y], cv.COLOR_GRAY2BGR)
-
-        imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank] * rows
-        hor_con = [imageBlank] * rows
-        for x in range(0, rows):
-            for y in range(0, cols):
-                if y < len(names[x]):  # Check if the name exists for this column
-                    cv.putText(imgArray[x][y], names[x][y], (10, 20), cv.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0, 0, 255), 2)
-            hor[x] = np.hstack(imgArray[x])
-        ver = np.vstack(hor)
-    else:
-        for x in range(0, rows):
-            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-                imgArray[x] = cv.resize(imgArray[x], (0, 0), None, scale, scale)
-            else:
-                imgArray[x] = cv.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
-            if len(imgArray[x].shape) == 2:
-                imgArray[x] = cv.cvtColor(imgArray[x], cv.COLOR_GRAY2BGR)
-        hor = np.hstack(imgArray)
-        ver = hor
-    return ver
 
 
 # 1. DISPLAY THE FRAME
